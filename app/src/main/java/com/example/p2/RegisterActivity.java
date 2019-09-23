74ff3b59d5a2960f5b7091208a5b3a63bb14d57d
dam2a <fdomin@elpuig.xeill.net>
2019-09-23 19:02:43 +0200

    dgfdgfd

---
+++ app/src/main/java/com/example/p2/RegisterActivity.java
@@ -0,0 +1,47 @@
+package com.example.p2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        Button botonRegistrar = findViewById(R.id.botonRegistrar);
+        botonRegistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText editTextUserName = findViewById(R.id.userName);
+                EditText editTextUserEmail = findViewById(R.id.userEmail);
+                EditText editTextUserPhone = findViewById(R.id.userPhone);
+                EditText editTextUserPassword = findViewById(R.id.userPassword);
+
+
+                String userName = editTextUserName.getText().toString();
+                String userEmail = editTextUserEmail.getText().toString();
+                String userPhone = editTextUserPhone.getText().toString();
+                String userPassword = editTextUserPassword.getText().toString();
+
+                Intent intent = new Intent(RegisterActivity.this, ShowInfoActivity.class);
+
+
+                intent.putExtra("USER_NAME", userName);
+                intent.putExtra("USER_EMAIL", userEmail);
+                intent.putExtra("USER_PHONE", userPhone);
+                intent.putExtra("USER_PASSWORD", userPassword);
+
+                startActivity(intent);
+            }
+
+        });
+
+    }
+}
